18fde55fa03347d572d7cbe88645accee6491384
Jivesh <jsoperatorz@gmail.com>
2023-03-09 10:07:27 +0530

    Project & Tailwind Setup

---
+++ layouts/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <header className='bg-black p-5 text-white'>
+                Header
+            </header>
+        </>
+    )
+}
+
+export default Header
